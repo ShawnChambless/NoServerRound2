4010e6540954b03f32bf9db1c38df93b9823b1f9
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-21 19:24:17 -0600

    tried things, reverted back.

--- scripts/controllers/homePage/homePageCtrl.js
+++ scripts/controllers/homePage/homePageCtrl.js
@@ -1,5 +1,17 @@
 var app = angular.module('noServer2')
 
-app.controller('homePageCtrl', function($scope, loginService) {
-    
+app.controller('homePageCtrl', function($scope, tripsService, $firebaseArray) {
+    var authData = tripsService.authData;
+    $scope.addTrip = function() {
+        tripsService.addTrip($scope.newTrip);
+    };
+
+    $scope.toPreviousTrips = function() {
+        tripsService.toPreviousTrips($scope.currentTrip);
+    };
+
+    var currentTripsRef = new Firebase('https://noserverproject2.firebaseio.com/users/' + authData.uid + '/trips/currentTrip');
+    var prevTripRef = new Firebase('https://noserverproject2.firebaseio.com/users/' + authData.uid + '/trips/previousTrips')
+        $scope.currentTrip = $firebaseArray(currentTripsRef);
+        $scope.previousTrips = $firebaseArray(prevTripRef);
 });
